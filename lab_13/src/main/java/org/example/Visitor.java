3b97a529d10cf6483766696793952a564568850a
Olli <91615532+linndfors@users.noreply.github.com>
2022-12-22 07:56:41 +0200

    Delete lab_13 directory

--- lab_13/src/main/java/org/example/Visitor.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.Map;
-
-public interface Visitor<T> {
-    Map<String, String> onSignature(Task<T> task);
-    Map<String, String> onGroupStart(Task<T> task);
-    Map<String, String> onGroupEnd(Task<T> task);
-}
